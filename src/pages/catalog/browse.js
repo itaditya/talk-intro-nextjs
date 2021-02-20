ec1ad74d399b3741801fd1e993d0b5b941eef6eb
Aditya Agarwal <adityaa803@gmail.com>
2021-02-20 18:12:30 +0530

    add nav_bar to home & browse pages

--- src/pages/catalog/browse.js
+++ src/pages/catalog/browse.js
@@ -1,6 +1,13 @@
+import { NavBar } from '../../components/nav_bar/nav_bar';
+
 function Browse() {
   return (
-    <div>Browse</div>
+    <div>
+      <NavBar />
+      <div>
+        <h1>Browse</h1>
+      </div>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
+import { NavBar } from '../components/nav_bar/nav_bar';
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <NavBar />
+      <div>
+        <h1>Home</h1>
+      </div>
+    </div>
   );
 }
 
