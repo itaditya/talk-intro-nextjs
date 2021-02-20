9604470742be4e3ee813931b55651b651d48ac80
Aditya Agarwal <adityaa803@gmail.com>
2021-02-20 18:47:03 +0530

    implement products_grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,20 @@
 import { NavBar } from '../components/nav_bar/nav_bar';
+import { ProductsGrid } from '../components/products_grid/products_grid';
+
+const products = [
+  {
+    id: 'cupiditate-qui-et',
+    name: 'Sleek Soft Table',
+    price: '809.00',
+    imageUrl: 'http://placeimg.com/640/480/technics?id=78896',
+  },
+  {
+    id: 'in-id-aliquid',
+    name: 'Fantastic Granite Gloves',
+    price: '952.00',
+    imageUrl: 'http://placeimg.com/640/480/technics?id=29157',
+  },
+];
 
 function Home() {
   return (
@@ -6,6 +22,7 @@ function Home() {
       <NavBar />
       <div>
         <h1>Home</h1>
+        <ProductsGrid products={products} />
       </div>
     </div>
   );
